794ae8adfc54e2a922b1bf259674b0874f87b1d4
iryu54 <corentinlabroche@gmail.com>
2016-11-07 14:40:59 +0100

    Update the database for kebab and users

--- _installation/migration.sql
+++ _installation/migration.sql
@@ -1,40 +1,17 @@
 SET FOREIGN_KEY_CHECKS = 0;
 SELECT GROUP_CONCAT(table_schema, '.', table_name) INTO @tables
   FROM information_schema.tables
-  WHERE table_schema = 'slim3'; -- specify DB name here.
+  WHERE table_schema = 'database'; -- specify DB name here.
 
 SET @tables = CONCAT('DROP TABLE ', @tables);
 PREPARE stmt FROM @tables;
 EXECUTE stmt;
 SET FOREIGN_KEY_CHECKS = 1;
 
--- phpMyAdmin SQL Dump
--- version 4.5.2
--- http://www.phpmyadmin.net
---
--- Client :  localhost
--- Généré le :  Ven 04 Novembre 2016 à 14:27
--- Version du serveur :  10.1.16-MariaDB
--- Version de PHP :  7.0.9
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de données :  `slim3`
---
-
--- --------------------------------------------------------
-
---
--- Structure de la table `adresses`
---
 
 CREATE TABLE `adresses` (
   `adresse_id` varchar(13) NOT NULL,
@@ -48,12 +25,6 @@ CREATE TABLE `adresses` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Structure de la table `comments`
---
-
 CREATE TABLE `comments` (
   `user_id` varchar(13) NOT NULL,
   `kebab_id` varchar(13) NOT NULL,
@@ -62,29 +33,16 @@ CREATE TABLE `comments` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Structure de la table `kebabs`
---
-
 CREATE TABLE `kebabs` (
   `kebab_id` varchar(13) NOT NULL,
   `user_id` varchar(13) NOT NULL,
   `kebab_description` varchar(1420) NOT NULL,
   `kebab_tasty_points` int(9) NOT NULL,
   `kebab_image_path` varchar(13) NOT NULL,
+  `kebab_image_extension` varchar(4) NOT NULL,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
--- --------------------------------------------------------
-
---
--- Structure de la table `tags`
---
 
 CREATE TABLE `tags` (
   `tag_id` varchar(13) NOT NULL,
@@ -93,12 +51,6 @@ CREATE TABLE `tags` (
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `users`
---
 
 CREATE TABLE `users` (
   `user_id` varchar(13) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'auto incrementing user_id of each user, unique index',
@@ -126,23 +78,6 @@ CREATE TABLE `users` (
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='user data';
 
---
--- Contenu de la table `users`
---
-
-INSERT INTO `users` (`user_id`, `session_id`, `user_name`, `user_age`, `user_gender`, `user_slug`, `user_password_hash`, `user_email`, `user_active`, `user_deleted`, `user_account_type`, `user_has_avatar`, `user_remember_me_token`, `user_suspension_timestamp`, `user_last_login_timestamp`, `user_failed_logins`, `user_last_failed_login`, `user_activation_hash`, `user_profile`, `user_password_reset_hash`, `user_password_reset_timestamp`, `created_at`, `updated_at`) VALUES
-('16\0\0\0\0\0\0\0\0\0\0\0\0\0\0', NULL, 'iryu54', 1, '', '', '$2y$10$grSmtBRI.W8YKU0JZudxgOXBLgHrsC4PeP1QCPCO9dN53p6Z.N4h2', 'corentinlabroche@gmail.com', 0, 0, 0, 0, NULL, NULL, NULL, 1, 1476777036, NULL, 1, NULL, NULL, '2016-10-17 12:09:27', '2016-10-18 05:50:36'),
-('19\0\0\0\0\0\0\0\0\0\0\0\0\0\0', '4054fue7iskkemkvtes6d3cvl2', 'aa', 0, 'Non Specified', '', '$2y$10$A38uwE1QSd0iR3Xks9jJIef.kNQkPrTu/iz6LNBlZdOFJDUdRlIla', 'a@a.a', 0, 0, 0, 0, NULL, NULL, NULL, 0, NULL, NULL, 1, NULL, NULL, '2016-10-18 05:16:04', '2016-10-19 10:35:08'),
-('3\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0', NULL, 'demo', 0, '', '', '$2y$10$w92ibsqyGjlGBL8vQipbJeS9BCUcbC8j0vxIgfC5ShxAdcsXC/s3W', 'john@example.com', 0, 0, 1, 0, NULL, NULL, NULL, 0, NULL, NULL, 1, NULL, NULL, '0000-00-00 00:00:00', '2016-10-12 06:07:02'),
-('4\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0', NULL, 'demo2', 0, '', '', '$2y$10$w92ibsqyGjlGBL8vQipbJeS9BCUcbC8j0vxIgfC5ShxAdcsXC/s3W', 'demo@example.com', 0, 0, 1, 0, NULL, NULL, NULL, 0, NULL, NULL, 1, NULL, NULL, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-('5\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0', NULL, 'mrSpam', 0, '', '', '$2y$10$w92ibsqyGjlGBL8vQipbJeS9BCUcbC8j0vxIgfC5ShxAdcsXC/s3W', 'spam.alot@example.com', 0, 1, 1, 0, NULL, NULL, NULL, 0, NULL, NULL, 1, NULL, NULL, '0000-00-00 00:00:00', '0000-00-00 00:00:00');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `vote`
---
-
 CREATE TABLE `vote` (
   `kebab_id` varchar(13) NOT NULL,
   `user_id` varchar(13) NOT NULL,
@@ -150,51 +85,44 @@ CREATE TABLE `vote` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Index pour les tables exportées
---
 
---
--- Index pour la table `adresses`
---
+
+
+
+
+
+
+
+
+
+
 ALTER TABLE `adresses`
   ADD PRIMARY KEY (`adresse_id`);
 
---
--- Index pour la table `comments`
---
 ALTER TABLE `comments`
   ADD PRIMARY KEY (`user_id`),
   ADD KEY `kebab_id` (`kebab_id`);
 
---
--- Index pour la table `kebabs`
---
 ALTER TABLE `kebabs`
   ADD PRIMARY KEY (`kebab_id`),
   ADD KEY `user_id` (`user_id`);
 
---
--- Index pour la table `tags`
---
 ALTER TABLE `tags`
   ADD PRIMARY KEY (`tag_id`);
 
---
--- Index pour la table `users`
---
 ALTER TABLE `users`
   ADD PRIMARY KEY (`user_id`),
   ADD UNIQUE KEY `user_name` (`user_name`),
   ADD UNIQUE KEY `user_email` (`user_email`);
 
---
--- Index pour la table `vote`
---
 ALTER TABLE `vote`
   ADD PRIMARY KEY (`kebab_id`),
   ADD KEY `user_id` (`user_id`);
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+
+
+
+INSERT INTO `users` (`user_id`, `session_id`, `user_name`, `user_age`, `user_gender`, `user_slug`, `user_password_hash`, `user_email`, `user_active`, `user_deleted`, `user_account_type`, `user_has_avatar`, `user_remember_me_token`, `user_suspension_timestamp`, `user_last_login_timestamp`, `user_failed_logins`, `user_last_failed_login`, `user_activation_hash`, `user_profile`, `user_password_reset_hash`, `user_password_reset_timestamp`, `created_at`, `updated_at`) VALUES
+('582082313cabe', NULL, 'iryu54', 0, 'Non Specified', '', '$2y$10$Nk/YWAztZWniGOKeJKZIyebYRT6bX9NCwPUtP09DRQ9ERbDN.RkDK', 'lementai@hotmail.fr', 0, 0, 0, 0, NULL, NULL, NULL, 0, NULL, NULL, 1, NULL, NULL, '2016-11-07 13:31:29', '2016-11-07 13:32:16');
