ecfb79e3577ba501c9dd1babc8a86ad72b445e89
iryu54 <corentinlabroche@gmail.com>
2016-11-12 20:24:00 +0100

    Change texts in database

--- _installation/migration.sql
+++ _installation/migration.sql
@@ -84,7 +84,7 @@ CREATE TABLE `vote` (
   `kebab_id` varchar(13) NOT NULL,
   `user_id` varchar(13) NOT NULL,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP 
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
@@ -102,8 +102,9 @@ ALTER TABLE `adresses`
   ADD PRIMARY KEY (`adresse_id`);
 
 ALTER TABLE `comments`
-  ADD PRIMARY KEY (`user_id`),
-  ADD KEY `kebab_id` (`kebab_id`);
+  ADD PRIMARY KEY (`comment_id`),
+  ADD KEY `kebab_id` (`kebab_id`),
+  ADD KEY `user_id` (`user_id`);
 
 ALTER TABLE `kebabs`
   ADD PRIMARY KEY (`kebab_id`),
@@ -128,48 +129,45 @@ ALTER TABLE `vote`
 
 
 INSERT INTO `users` (`user_id`, `session_id`, `user_name`, `user_age`, `user_gender`, `user_slug`, `user_password_hash`, `user_email`, `user_active`, `user_deleted`, `user_account_type`, `user_has_avatar`, `user_remember_me_token`, `user_suspension_timestamp`, `user_last_login_timestamp`, `user_failed_logins`, `user_last_failed_login`, `user_activation_hash`, `user_profile`, `user_password_reset_hash`, `user_password_reset_timestamp`, `updated_at`, `created_at`) VALUES
-('582082313cabe', NULL, 'iryu54', 0, 'Non Specified', '', '$2y$10$Nk/YWAztZWniGOKeJKZIyebYRT6bX9NCwPUtP09DRQ9ERbDN.RkDK', 'lementai@hotmail.fr', 0, 0, 0, 0, NULL, NULL, NULL, 0, NULL, NULL, 1, NULL, NULL, '2016-11-07 13:11:55', '2016-11-07 14:11:55'),
 ('58208e831f706', NULL, 'JeanTourloupe', 0, 'Non Specified', '', '$2y$10$0Fv05VqaVt4wBQsHPwsJauNZvF0kqu1HRkf.ypUc.b8hTlZVe/8aO', 'bernard@ladrigotte.onche', 0, 0, 0, 0, NULL, NULL, NULL, 0, NULL, NULL, 1, NULL, NULL, '2016-11-07 12:24:25', '2016-11-07 13:24:25'),
-('58208ef3b223c', NULL, 'a', 0, 'Non Specified', '', '$2y$10$P2/qBNfKdVDHIih8vh77H.P0KBK9I3iOd7HhWASD/22uSYrlpvEAS', 'a@a.a', 0, 0, 0, 0, NULL, NULL, NULL, 0, NULL, NULL, 1, NULL, NULL, '2016-11-07 12:48:38', '2016-11-07 13:48:38'),
 ('58209d4064b77', 'ltjmb7iu8132osh36j7cadi0j6', 'Bernard', 30, 'Male', '', '$2y$10$Rg/uJqJygI96fnxxSo0eeeT4tAOOZlKS9lbzu96fftDccEn4Jlr6q', 'bernard@ladrigotte.ponche', 0, 0, 0, 0, NULL, NULL, NULL, 0, NULL, NULL, 1, NULL, NULL, '2016-11-11 13:37:16', '2016-11-10 12:39:09'),
-('58220576c9787', NULL, 'JeanFlibuste', 31, 'Non Specified', '', '$2y$10$cuylZQ7ElcdK1FNnmVThHuGg2H4zz/sOddU/AlOS8RmC5ZaOtFfBm', 'ilapas@ditbonjour.vald', 0, 0, 0, 0, NULL, NULL, NULL, 0, NULL, NULL, 1, NULL, NULL, '2016-11-09 15:46:55', '2016-11-09 16:46:55'),
-('5824e7f5e92ff', NULL, 'essai2', 14, 'Male', '', '$2y$10$YmguK8pGEPdnoPN9eWwFyOLrEK1kHgajuPgWsH.48BdMirvA3ORrO', 'essai2@u.a', 0, 0, 0, 0, NULL, NULL, NULL, 0, NULL, NULL, 1, NULL, NULL, '2016-11-10 20:49:34', '2016-11-10 20:49:34');
+('58220576c9787', NULL, 'JeanFlibuste', 31, 'Non Specified', '', '$2y$10$cuylZQ7ElcdK1FNnmVThHuGg2H4zz/sOddU/AlOS8RmC5ZaOtFfBm', 'ilapas@ditbonjour.vald', 0, 0, 0, 0, NULL, NULL, NULL, 0, NULL, NULL, 1, NULL, NULL, '2016-11-09 15:46:55', '2016-11-09 16:46:55');
 
 
 INSERT INTO `kebabs` (`kebab_id`, `user_id`, `kebab_description`, `kebab_tasty_points`, `kebab_image_path`, `kebab_image_extension`, `updated_at`, `created_at`) VALUES
-('581cf0ccec0aa', '58209d4064b77', 'ISSOU', 5, '581cf0ccec0b0', 'jpg', '2016-11-08 15:59:34', '2016-11-08 15:59:34'),
-('581d05e46f9c5', '58209d4064b77', 'Un kebab asiat parce que c\'est du chien kappa', 1, '581d05e46f9c9', 'jpg', '2016-11-11 13:37:52', '2016-11-08 15:59:23'),
-('581f61f03b542', '58209d4064b77', 'Un jour de printemps, un grand philosophe eut la sympathie de m\'octroyer un délicieux kebab, j\'ai juré wallah', 0, '581f61f03b549', 'png', '2016-11-11 13:38:21', '2016-11-08 15:59:16'),
-('582095fbd6b5f', '58209d4064b77', 'Kebab sans salade, sans tomate, sans sauce, sans pain.', 0, '582095fbd6b66', 'jpg', '2016-11-08 15:59:05', '2016-11-08 15:59:05'),
-('5820b209251d1', '58209d4064b77', 'AH ! Un kebab !', 0, '5820b209251d8', 'jpg', '2016-11-07 14:55:37', '2016-11-07 14:55:37'),
-('582206f9be983', '58220576c9787', 'Don\'t believe in the me who believe in you, or the you who believe in me, believe in the you that believe in you ! WHO THE HELL DO YOU THINK YOU ARE ???', 1, '582206f9be989', 'jpg', '2016-11-11 13:41:59', '2016-11-08 15:10:17');
+('581cf0ccec0aa', '58209d4064b77', 'Ca fait longtemps que j\'en ai pas mangé,humm un bon kebab', 5, '581cf0ccec0b0', 'jpg', '2016-11-08 15:59:34', '2016-11-08 15:59:34'),
+('581d05e46f9c5', '58209d4064b77', 'AU TOP la soirée avec les potes à manger celui-ci', 1, '581d05e46f9c9', 'jpg', '2016-11-11 13:37:52', '2016-11-08 15:59:23'),
+('581f61f03b542', '58209d4064b77', 'Un jour de printemps, un grand philosophe eut la sympathie de m\'octroyer un délicieux kebab', 0, '581f61f03b549', 'png', '2016-11-11 13:38:21', '2016-11-08 15:59:16'),
+('582095fbd6b5f', '58209d4064b77', 'Kebab tomate oignon salade.', 0, '582095fbd6b66', 'jpg', '2016-11-08 15:59:05', '2016-11-08 15:59:05'),
+('5820b209251d1', '58209d4064b77', 'Fidèle à la maison depuis bientôt 8 ans pour rien au monde on irait ailleurs.', 0, '5820b209251d8', 'jpg', '2016-11-07 14:55:37', '2016-11-07 14:55:37'),
+('582206f9be983', '58220576c9787', 'Super Kebab', 1, '582206f9be989', 'jpg', '2016-11-11 13:41:59', '2016-11-08 15:10:17');
 
 INSERT INTO `adresses` (`adresse_id`, `shop_name`, `country`, `city`, `street_number`, `kebab_id`, `street`, `updated_at`, `created_at`) VALUES
-('581cf0cd4ac3f', 'OOPOPOO', 'OPOPOPO', 'POPOPO', 7, '581cf0ccec0aa', 'POPOPO', '2016-11-04 17:34:21', '2016-11-04 17:34:21'),
-('581d05e4978dd', 'LeePong et fils', 'Chine', 'HongKong', 420, '581d05e46f9c5', 'Rue de la MLG', '2016-11-04 19:04:20', '2016-11-04 19:04:20'),
-('581f61f0dea89', 'JPC Kebab', 'France', 'Onche City', 8173, '581f61f03b542', 'If you know what i mean', '2016-11-06 14:01:36', '2016-11-06 14:01:36'),
-('582095fbf224b', 'JPC Kebab', 'DTC', 'MDR', 450, '582095fbd6b5f', 'REKT', '2016-11-07 12:55:55', '2016-11-07 12:55:55'),
-('5820b2094d68f', 'Ile Hawai', 'PACIFIQUE', 'DTC', 4545454, '5820b209251d1', 'ONCHE', '2016-11-07 14:55:37', '2016-11-07 14:55:37'),
-('582206fa3bf77', 'Simon', 'TheHeavens', 'Tepelinn', 9001, '582206f9be983', 'rue de la brutasserie', '2016-11-08 15:10:18', '2016-11-08 15:10:18');
+('581cf0cd4ac3f', 'Kebabia', 'Pologne', 'Rydułtowy', 55, '581cf0ccec0aa', 'ul. Ofiar Terroru', '2016-11-04 17:34:21', '2016-11-04 17:34:21'),
+('581d05e4978dd', 'LeePong et fils', 'Espagne', 'Castelao', 32, '581d05e46f9c5', 'Rue de la MLG', '2016-11-04 19:04:20', '2016-11-04 19:04:20'),
+('581f61f0dea89', 'JPC Kebab', 'France', 'SAINT-ANDRÉ', 20, '581f61f03b542', 'rue de l\'Epeule', '2016-11-06 14:01:36', '2016-11-06 14:01:36'),
+('582095fbf224b', 'JPC Kebab', 'Deutschland', 'Mindelstetten', 450, '582095fbd6b5f', 'AlleestraBe', '2016-11-07 12:55:55', '2016-11-07 12:55:55'),
+('5820b2094d68f', 'Norvegia Kebab', 'Norvege', 'Mühloz', 8, '5820b209251d1', 'Gamelevegen', '2016-11-07 14:55:37', '2016-11-07 14:55:37'),
+('582206fa3bf77', 'Simon Kebab', 'Danemark', 'Bredsten', 9001, '582206f9be983', 'Serup Skowej', '2016-11-08 15:10:18', '2016-11-08 15:10:18');
 
 INSERT INTO `tags` (`tag_id`, `kebab_id`, `tag_text`, `updated_at`, `created_at`) VALUES
-('581cf0cd45c04', '581cf0ccec0aa', 'ISSOU', '2016-11-04 17:34:21', '2016-11-04 17:34:21'),
-('581d05e47f655', '581d05e46f9c5', 'Cliche', '2016-11-04 19:04:20', '2016-11-04 19:04:20'),
-('581d05e4842df', '581d05e46f9c5', 'doge', '2016-11-04 19:04:20', '2016-11-04 19:04:20'),
-('581d05e48c6af', '581d05e46f9c5', 'onche', '2016-11-04 19:04:20', '2016-11-04 19:04:20'),
-('581f61f078797', '581f61f03b542', 'JPC', '2016-11-06 14:01:36', '2016-11-06 14:01:36'),
-('581f61f095dfa', '581f61f03b542', 'YTP', '2016-11-06 14:01:36', '2016-11-06 14:01:36'),
-('581f61f0b64cb', '581f61f03b542', 'kebab', '2016-11-06 14:01:36', '2016-11-06 14:01:36'),
-('581f61f0cded6', '581f61f03b542', 'unboeuf', '2016-11-06 14:01:36', '2016-11-06 14:01:36'),
-('581f61f0d8bbe', '581f61f03b542', 'nonprobleme', '2016-11-06 14:01:36', '2016-11-06 14:01:36'),
-('582095fbe1c7d', '582095fbd6b5f', 'Boeuf', '2016-11-07 12:55:55', '2016-11-07 12:55:55'),
-('582095fbea4c8', '582095fbd6b5f', 'Meuh', '2016-11-07 12:55:55', '2016-11-07 12:55:55'),
-('5820b2093594a', '5820b209251d1', 'kohlanta', '2016-11-07 14:55:37', '2016-11-07 14:55:37'),
-('5820b2093f342', '5820b209251d1', 'danny', '2016-11-07 14:55:37', '2016-11-07 14:55:37'),
-('5820b20945565', '5820b209251d1', 'kebab', '2016-11-07 14:55:37', '2016-11-07 14:55:37'),
-('582206f9ee4ba', '582206f9be983', 'TTGL', '2016-11-08 15:10:17', '2016-11-08 15:10:17'),
-('582206fa25c7b', '582206f9be983', 'drill', '2016-11-08 15:10:18', '2016-11-08 15:10:18'),
-('582206fa2d110', '582206f9be983', 'GigaDrillBreaker', '2016-11-08 15:10:18', '2016-11-08 15:10:18');
+('581cf0cd45c04', '581cf0ccec0aa', 'tag1', '2016-11-04 17:34:21', '2016-11-04 17:34:21'),
+('581d05e47f655', '581d05e46f9c5', 'tag2', '2016-11-04 19:04:20', '2016-11-04 19:04:20'),
+('581d05e4842df', '581d05e46f9c5', 'tag3', '2016-11-04 19:04:20', '2016-11-04 19:04:20'),
+('581d05e48c6af', '581d05e46f9c5', 'tag4', '2016-11-04 19:04:20', '2016-11-04 19:04:20'),
+('581f61f078797', '581f61f03b542', 'tag5', '2016-11-06 14:01:36', '2016-11-06 14:01:36'),
+('581f61f095dfa', '581f61f03b542', 'tag6', '2016-11-06 14:01:36', '2016-11-06 14:01:36'),
+('581f61f0b64cb', '581f61f03b542', 'tag7', '2016-11-06 14:01:36', '2016-11-06 14:01:36'),
+('581f61f0cded6', '581f61f03b542', 'tag8', '2016-11-06 14:01:36', '2016-11-06 14:01:36'),
+('581f61f0d8bbe', '581f61f03b542', 'tag9', '2016-11-06 14:01:36', '2016-11-06 14:01:36'),
+('582095fbe1c7d', '582095fbd6b5f', 'tag10', '2016-11-07 12:55:55', '2016-11-07 12:55:55'),
+('582095fbea4c8', '582095fbd6b5f', 'tag11', '2016-11-07 12:55:55', '2016-11-07 12:55:55'),
+('5820b2093594a', '5820b209251d1', 'tag12', '2016-11-07 14:55:37', '2016-11-07 14:55:37'),
+('5820b2093f342', '5820b209251d1', 'tag13', '2016-11-07 14:55:37', '2016-11-07 14:55:37'),
+('5820b20945565', '5820b209251d1', 'tag14', '2016-11-07 14:55:37', '2016-11-07 14:55:37'),
+('582206f9ee4ba', '582206f9be983', 'tag15', '2016-11-08 15:10:17', '2016-11-08 15:10:17'),
+('582206fa25c7b', '582206f9be983', 'tag16', '2016-11-08 15:10:18', '2016-11-08 15:10:18'),
+('582206fa2d110', '582206f9be983', 'tag17', '2016-11-08 15:10:18', '2016-11-08 15:10:18');
 
 INSERT INTO `vote` (`id`, `kebab_id`, `user_id`, `updated_at`, `created_at`) VALUES
 ('5824eb145236c', '58246ff2995ea', '582082313cabe', '2016-11-10 20:48:04', '2016-11-10 20:48:04'),
@@ -186,5 +184,4 @@ INSERT INTO `vote` (`id`, `kebab_id`, `user_id`, `updated_at`, `created_at`) VAL
 
 INSERT INTO `comments` (`comment_id`, `user_id`, `kebab_id`, `texte`, `updated_at`, `created_at`) VALUES
 ('582604fa40dc2', '58209d4064b77', '581d05e46f9c5', 'c\'est trop croustillant', '2016-11-11 16:50:50', '2016-11-11 16:50:50'),
-('58260d11ba363', '58209d4064b77', '581f61f03b542', 'Le problème du temps est un non problème !', '2016-11-11 17:25:21', '2016-11-11 17:25:21'),
-('58260d25ab374', '58209d4064b77', '581f61f03b542', 'Caca dans des raviolis, ça vous va ?', '2016-11-11 17:25:41', '2016-11-11 17:25:41');
+('58260d11ba363', '58209d4064b77', '581f61f03b542', 'Ca donne faim !', '2016-11-11 17:25:21', '2016-11-11 17:25:21');
